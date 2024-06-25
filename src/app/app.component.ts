413e76a2d47deffad38bfb14fbfd9a5e2560d659
victorscdev <victorsc.job@gmail.com>
2024-06-25 19:50:17 -0300

    Criando sistema base de rotas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: 
+  `
+    <header-component></header-component>
+    <router-outlet></router-outlet>
+    <footer-component></footer-component>
+  `
 })
 export class AppComponent {
   title = 'avaliacao';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponentModule } from './component/header/header.module';
+import { FooterComponentModule } from './component/footer/footer.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HeaderComponentModule,
+    FooterComponentModule
   ],
   providers: [],
   bootstrap: [AppComponent]
